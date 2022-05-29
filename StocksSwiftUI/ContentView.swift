0868958fa6ad01958224cc793b2e17a2b961ed35
Sean <seanbopo@gmail.com>
2022-05-29 13:31:21 +0800

    Initial Commit

---
+++ StocksSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksSwiftUI/StocksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksSwiftUIApp.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/5/29.
+//
+
+import SwiftUI
+
+@main
+struct StocksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
