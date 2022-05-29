339086f857272a5aa0a6f7aebb9e673aff39b29b
Sean <seanbopo@gmail.com>
2022-05-29 18:21:38 +0800

    Implementing StockListViewModel and StockViewModel

---
+++ StocksSwiftUI/ViewModels/StockListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  StockListViewModel.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/5/29.
+//
+
+import Foundation
+
+
+class stockListViewModel {
+    
+    var searchTerm: String = ""
+    var stocks: [StockViewModel] = [StockViewModel]()
+    
+}
