b759165f54c6dc97fecb34f9da44d88184185b5a
Sean <seanbopo@gmail.com>
2022-05-29 22:11:04 +0800

    Setting UP User Interface in SwiftUI

--- StocksSwiftUI/ContentView.swift
+++ StocksSwiftUI/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchTerm: String = ""
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = .black
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor = .black
+        UITableViewCell.appearance().backgroundColor = .black
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .leading) {
+                Color.black
+                
+                Text("January 5 2020")
+                    .font(.custom("Arial", size: 32))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.gray)
+                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
+                    .offset(y: -265)
+                
+                SearchView(searchTerm: $searchTerm)
+                    .offset(y: -220)
+            
+                    .navigationBarTitle("Stocks")
+                    .edgesIgnoringSafeArea(.top)
+            }.ignoresSafeArea(edges: [.bottom, .trailing, .top])
+            // .edgesIgnoringSafeArea(Edge.Set(.bottom))
+        }
     }
 }
 
