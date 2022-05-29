f4e47217ea87ed2e249d09e4e6cf368ce9276761
Sean <seanbopo@gmail.com>
2022-05-29 14:10:52 +0800

    Implementing the Webservice and Stocks Model

---
+++ StocksSwiftUI/Models/Stock.swift
@@ -0,0 +1,8 @@
+//
+//  Stock.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/5/29.
+//
+
+import Foundation
---
+++ StocksSwiftUI/Services/Webservice.swift
@@ -0,0 +1,8 @@
+//
+//  Webservice.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/5/29.
+//
+
+import Foundation
