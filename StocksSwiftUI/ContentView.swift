0fa52bb4069df74f958ec1a438e10892ff9e9431
Sean <seanbopo@gmail.com>
2022-06-04 20:09:46 +0800

    - Displaying Stocks in a list view - Filtering Stock

--- StocksSwiftUI/ContentView.swift
+++ StocksSwiftUI/ContentView.swift
@@ -8,35 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchTerm: String = ""
+    @ObservedObject private var stockListVM = StockListViewModel()
     
     init() {
         UINavigationBar.appearance().backgroundColor = .black
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         UITableView.appearance().backgroundColor = .black
         UITableViewCell.appearance().backgroundColor = .black
+        
+        stockListVM.load()
     }
     
     var body: some View {
-        NavigationView {
+        
+        let filteredStocks = self.stockListVM.searchTerm.isEmpty ? self.stockListVM.stocks : self.stockListVM.stocks.filter { $0.symbol.starts(with: self.stockListVM.searchTerm) }
+        
+        return NavigationView {
+                    
             ZStack(alignment: .leading) {
                 Color.black
-                
+                        
                 Text("January 5 2020")
                     .font(.custom("Arial", size: 32))
                     .fontWeight(.bold)
                     .foregroundColor(Color.gray)
-                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
-                    .offset(y: -265)
-                
-                SearchView(searchTerm: $searchTerm)
-                    .offset(y: -220)
-            
-                    .navigationBarTitle("Stocks")
-                    .edgesIgnoringSafeArea(.top)
-            }.ignoresSafeArea(edges: [.bottom, .trailing, .top])
-            // .edgesIgnoringSafeArea(Edge.Set(.bottom))
-        }
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    .offset(y: -210)
+                        
+                SearchView(searchTerm: self.$stockListVM.searchTerm)
+                    .offset(y: -170)
+                        
+                StockListView(stocks: filteredStocks)
+                    .offset(y: 90)
+            }
+                    
+            .navigationBarTitle("Stocks")
+        }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
--- StocksSwiftUI/ViewModels/StockListViewModel.swift
+++ StocksSwiftUI/ViewModels/StockListViewModel.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 
-class stockListViewModel: ObservableObject{
+class StockListViewModel: ObservableObject{
     
-    var searchTerm: String = ""
+    @Published var searchTerm: String = ""
     @Published var stocks: [StockViewModel] = [StockViewModel]()
     
     func load() {
+//        let stock = Stock(symbol: "GOOG", description: "Google Stocks", price: 1200, change: "+0.23")
+//        self.stocks.append(StockViewModel(stock:stock))
+//        self.stocks.append(StockViewModel(stock:stock))
+//        self.stocks.append(StockViewModel(stock:stock))
+//        return StockListView(stocks: [StockViewModel(stock:stock)])
         fetchStocks()
     }
     
@@ -22,6 +27,13 @@ class stockListViewModel: ObservableObject{
             if let stocks = stocks {
                 DispatchQueue.main.async {
                     self.stocks = stocks.map(StockViewModel.init)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.stocks.append(StockViewModel(stock:Stock(symbol: "GOOG", description: "Google Stocks", price: 1200, change: "+0.23")))
+                    self.stocks.append(StockViewModel(stock:Stock(symbol: "MSFT", description: "Microsoft Stocks", price: 600, change: "+0.3")))
+                    self.stocks.append(StockViewModel(stock:Stock(symbol: "FB", description: "Facebook Stocks", price: 1800, change: "+1.3")))
+                    self.stocks.append(StockViewModel(stock:Stock(symbol: "APPLE", description: "Apple Stocks", price: 1500, change: "+2.3")))
                 }
             }
         }
