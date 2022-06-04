32e5bd2a221c6bef47691c6a49af0f98a42256f7
Sean <seanbopo@gmail.com>
2022-06-04 20:24:46 +0800

    feat: Implementing Webservice to Fetch News and Creating Models

---
+++ StocksSwiftUI/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  StocksSwiftUI
+//
+//  Created by ChaoBo-Shang on 2022/6/4.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let imageURL: String
+    let publication: String
+}
